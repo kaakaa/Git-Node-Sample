9cc97e24db7b77b552b425da7d9f606a9769030e
kaakaa <stooner.hoe@gmail.com>
2013-11-26 23:16:45 +0900

    'commit'

--- server.js
+++ server.js
@@ -1,19 +1,29 @@
 var http = require('http'),
-	server = http.createServer();
+	server = http.createServer(),
+	fs = require('fs');
 
 
-function exec(req, res) {
+function commit(req, res) {
+	fs.appendFileSync('data.txt', 'commit!\n');
+	console.log('append!');
 	var sys = require('sys');
 	var exec = require('child_process').exec;
+	var out = [];
 	function puts(error, stdout, stderr) {
 		sys.puts(stdout)
-		res.writeHead(200, {'Content-Type': 'text/plain'});
-		res.write(stdout);
-		res.end();
 	};
-	exec("git status", puts)
+	exec("git init", puts);
+	console.log('git!');
+	exec("git add .", puts);
+	console.log('add!');
+	exec("git commit -m 'commit'", puts);
+	console.log('commit!');
+
+	res.writeHead(200, {'Content-Type': 'text/plain'});
+	res.write("commit ok\n");
+	res.end();
 }
 
-server.on('request', exec);
+server.on('request', commit);
 
 server.listen(3000);
